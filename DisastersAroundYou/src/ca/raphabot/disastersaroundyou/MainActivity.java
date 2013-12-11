5def3c3de5c937e25f5a0bba0db9000cb9764c23
raphabot <raphabot@gmail.com>
2013-12-11 01:06:01 -0500

    Maps working. Portrait and Landscape working.

--- DisastersAroundYou/src/ca/raphabot/disastersaroundyou/MainActivity.java
+++ DisastersAroundYou/src/ca/raphabot/disastersaroundyou/MainActivity.java
@@ -3,15 +3,12 @@ package ca.raphabot.disastersaroundyou;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.Toast;
 
-//import com.google.android.gms.maps.CameraUpdateFactory;
-//import com.google.android.gms.maps.GoogleMap;
-//import com.google.android.gms.maps.MapFragment;
-//import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-//import com.google.android.gms.maps.model.LatLng;
-//import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
 
 public class MainActivity extends Activity {
@@ -19,36 +16,41 @@ public class MainActivity extends Activity {
 	ListDisastersFragment listFragment;
 	ArrayList<String> disastersTitles = new ArrayList<String>();
 	private GoogleMap googleMap;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		//if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+		//      finish();
+		//      return;
+		 //   }
+		
 		setContentView(R.layout.activity_main);
 	}
-	
+
 	public void onItemClicked(int position) { 
 		//listFragment.showMap(position); 
 		try {
-            // Loading map
-           // initilizeMap();
- 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+			// Loading map
+			initilizeMap();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	} 
-	/*
+
 	private void initilizeMap() {
-        if (googleMap == null) {
-            googleMap = ((MapFragment) getFragmentManager().findFragmentById(
-                    R.id.map)).getMap();
- 
-            // check if map is created successfully or not
-            if (googleMap == null) {
-                Toast.makeText(getApplicationContext(),
-                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
-                        .show();
-            }
-        }
-    }
-*/
+		if (googleMap == null) {
+			googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+					R.id.map)).getMap();
+
+			// check if map is created successfully or not
+			if (googleMap == null) {
+				Toast.makeText(getApplicationContext(),
+						"Sorry! unable to create maps", Toast.LENGTH_SHORT).show();
+			}
+		}
+	}
+
 }
